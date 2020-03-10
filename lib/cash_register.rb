169c47fca68f9d20403622267406e70dbd188ca7
Matt Kuras <mattkuras@gmail.com>
2020-03-10 19:55:27 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -10,7 +10,7 @@ class CashRegister
   
   def add_item(title, price, quantity = 1)
        # binding.pry 
-    @total += price*quantity
+#    @total += price*quantity
     binding.pry 
   end
     
